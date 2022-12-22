e15f7210cf7b8afec2e41e435fcd2e3c8bd52678
ganovelli <fabio.ganovelli@gmail.com>
2022-12-22 17:17:40 +0100

    first commit
    glwf+glew

---
+++ src/code_0_setup/main.cpp
@@ -0,0 +1,40 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+ 
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1000, 800, "My Race", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+ 
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
