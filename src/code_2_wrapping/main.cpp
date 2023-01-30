07f70ea65331386e54e5049f1aa4cc521ccc41de
ganovelli <fabio.ganovelli@gmail.com>
2023-01-30 21:59:01 +0100

    code_1 and code_2

--- src/code_2_wrapping/main.cpp
+++ src/code_2_wrapping/main.cpp
@@ -17,7 +17,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1000, 800, "code_1_my_first_triangle", NULL, NULL);
+    window = glfwCreateWindow(1000, 800, "code_2_wrapping", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -31,55 +31,56 @@ int main(void)
     glewInit();
 
     printout_opengl_glsl_info();
-    GLuint va;
-    glGenVertexArrays(1, &va);
-    glBindVertexArray(va);
 
     /* create render data in RAM */
     GLuint positionAttribIndex = 0;
-    float positions[] = {   0.0, 0.0,  // 1st vertex
-                            0.5, 0.0,  // 2nd vertex
-                            0.5, 0.5,  // 3nd vertex
-                            0.0, 0.5  // 4th vertex
+    float positions[] = {   -0, -0.0,	// 1st vertex
+                            0.5, -0.0,  // 2nd vertex
+                            0.5, 0.5,	// 3nd vertex
+                            0.0, 0.5    // 4th vertex
     };
     renderable r;
     r.create();
 
-    r.add_vertex_attribute<float>(positions, 8, positionAttribIndex, 2);
+	r.add_vertex_attribute<float>(positions, 8, positionAttribIndex, 2);
+	check_gl_errors(__LINE__, __FILE__);
 
     GLuint colorAttribIndex = 1;
     float colors[] =      { 1.0, 0.0, 0.0,  // 1st vertex
                             0.0, 1.0, 0.0,  // 2nd vertex
-                            0.0, 0.0, 1.0,   // 3rd vertex
+                            0.0, 0.0, 1.0,  // 3rd vertex
                             1.0, 1.0, 1.0   // 4th vertex
     };
     r.add_vertex_attribute<float>(colors, 12, colorAttribIndex, 3);
+	check_gl_errors(__LINE__, __FILE__);
 
 
     GLuint indices[] = { 0,1,2,0,2,3 };
-    r.add_indices(indices, 6, GL_TRIANGLES);
+    r.add_indices(indices, 3, GL_TRIANGLES);
 
-    shader basic_shader;
+	shader basic_shader;
     basic_shader.create_program("shaders/basic.vert", "shaders/basic.frag");
+	basic_shader.bind("uDelta");
 	check_shader(basic_shader.vs);
 	check_shader(basic_shader.fs);
-
     validate_shader_program(basic_shader.pr);
 
-    r.bind();
+	check_gl_errors(__LINE__, __FILE__);
 
-    check_gl_errors(__LINE__, __FILE__);
+	r.bind();
+
+	int it = 0;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+		it++;
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(basic_shader.pr);
 
-       glUseProgram(basic_shader.pr);
-       
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-         
-        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, &indices[3]);
+       /* update the value of uDelta in the fragment shader */
+		glUniform1f(basic_shader["uDelta"], (it % 100) / 200.0);
+
         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);
         check_gl_errors(__LINE__,__FILE__);
         glUseProgram(0);
--- src/common/shaders.h
+++ src/common/shaders.h
@@ -24,6 +24,10 @@ struct shader{
 		std::ifstream ifs(fn);
 		std::string content((std::istreambuf_iterator<char>(ifs)),
 			(std::istreambuf_iterator<char>()));
+		if (content.empty()) {
+			std::cout << "No content for " << fn << std::endl;
+			exit(0);
+		}
 		return content;
 		}
 
